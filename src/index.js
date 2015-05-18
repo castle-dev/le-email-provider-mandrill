7e838cb93d27b6b28d215abcf2225491e558edca
Scott Lowe <scott@entercastle.com>
2015-05-18 01:32:16 -0400

    feat(EmailProvider): add sendHtml method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-module.exports = {};
+var q = require('q');
+var powerdrill = require('powerdrill');
+
+var EmailProvider = function (apiKey) {
+  if (!apiKey) { throw new Error('API key required'); }
+  var _api = powerdrill(apiKey);
+
+  this.sendHtml = function (from, to, subject, html) {
+    var deferred = q.defer();
+    var message = _api()
+    .subject(subject)
+    .to(to)
+    .from(from)
+    .html(html)
+    .send(function (err, resp) {
+      if (err) { deferred.reject(err); }
+      else if (resp[0].status === 'rejected') {
+        deferred.reject(new Error('Email rejected'));
+      } else { deferred.resolve(); }
+    });
+    return deferred.promise;
+  }
+}
+
+module.exports = EmailProvider;
