508fbd7fcc472dabd26c73ff3f7a27e37211784f
Scott Lowe <scott@entercastle.com>
2015-05-18 02:22:20 -0400

    feat(EmailProvider): send html and template emails

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ var powerdrill = require('powerdrill');
 var EmailProvider = function (apiKey) {
   if (!apiKey) { throw new Error('API key required'); }
   var _api = powerdrill(apiKey);
+  var handleResponse = function (err, resp, deferred) {
+      if (err) { deferred.reject(err); }
+      else if (resp.status === 'error') {
+        deferred.reject(new Error(resp.message));
+      } else if (resp[0] && resp[0].status === 'invalid') {
+        deferred.reject(new Error('Invalid email'));
+      } else if (resp[0] && resp[0].status === 'rejected') {
+        deferred.reject(new Error('Email rejected'));
+      } else if (resp[0] && resp[0].status === 'sent') {
+        deferred.resolve();
+      } else { deferred.reject(new Error('Uknown error')); }
+  }
 
   this.sendHtml = function (from, to, subject, html) {
     var deferred = q.defer();
@@ -13,13 +25,29 @@ var EmailProvider = function (apiKey) {
     .from(from)
     .html(html)
     .send(function (err, resp) {
-      if (err) { deferred.reject(err); }
-      else if (resp[0].status === 'rejected') {
-        deferred.reject(new Error('Email rejected'));
-      } else { deferred.resolve(); }
+      handleResponse(err, resp, deferred);
     });
     return deferred.promise;
-  }
+  };
+
+  this.sendTemplate = function (from, to, id, data) {
+    var deferred = q.defer();
+    var message = _api()
+    .to(to)
+    .from(from)
+    .template(id);
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        message.templateContent(key, data[key]);
+      }
+    }
+    message.send(function (err, resp) {
+      handleResponse(err, resp, deferred);
+    });
+    return deferred.promise
+  };
+
+  
 }
 
 module.exports = EmailProvider;
